857a6846beb9c2a9b6b180919a520e0e3483027f
Cheam Chanphearun <phearun6600@gmail.com>
2025-03-27 14:11:50 +0700

    update

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -3,13 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-class SkillController extends Controller
-{
-    namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
 
 class SkillController extends Controller
 {
