b235692d993173a36758a251fa768e2ef42047b2
Cheam Chanphearun <phearun6600@gmail.com>
2025-03-31 16:07:39 +0700

    update portfolio controller

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -76,7 +76,7 @@ class PortfolioController extends Controller
         foreach ($achievements as $achievement) {
             $achievement->endorsers = DB::table('achievement_endorsers')
                 ->join('users', 'achievement_endorsers.user_id', '=', 'users.google_id')
-                ->select('users.google_id', 'users.name')
+                ->select('users.google_id', 'users.name', 'users.email', 'endorsement_statuses.id as status_id', 'endorsement_statuses.status')
                 ->where('achievement_endorsers.achievement_id', $achievement->id)
                 ->get();
         }
